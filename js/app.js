80b37bc22f985d3baa8bbbd321dd8fdc2da8a170
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-09-05 22:34:24 -0700

    added 3 routes and used ui-sref

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 console.log('app.js is linked');
 var app = angular.module('sampleApp', ['ui.router']);
-app.config(['$stateProvider', function($stateProvider) {
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
   $stateProvider.state('firstMessage', {
     url: '/first-msg',
-    template: '<strong>first message</strong>'
-    //controller: 'first'
+    templateUrl: 'first.html',
+    controller: 'first'
+  })
+  .state('secondMessage', {
+    url: '/second-msg',
+    templateUrl: 'second.html',
+    controller: 'second'
+  })
+  .state('root', {
+    url: '/',
+    templateUrl: 'home.html'
   });
+  $urlRouterProvider.otherwise('/')
 }]);
-// app.controller('first', ['$scope', function($scope) {
-//   $scope.a = 10;
-//   $scope.b = 20;
-// }]);
+app.controller('first', ['$scope', function($scope) {
+  $scope.a = 10;
+  $scope.b = 20;
+}]);
+app.controller('second', ['$scope', function($scope) {
+  $scope.c = 20;
+  $scope.d = 30;
+}]);
